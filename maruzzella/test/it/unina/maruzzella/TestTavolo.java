84c286180b34343b8c6828f48cd07d1de601ef9e
saverix <saverix@c82b02a6-e1a1-5eb2-583e-98feb170680a>
2010-07-20 14:22:51 +0000

    Classe TestTavolo con Test2 => REDLINE

--- maruzzella/test/it/unina/maruzzella/TestTavolo.java
+++ maruzzella/test/it/unina/maruzzella/TestTavolo.java
@@ -13,7 +13,6 @@ public class TestTavolo {
 	 * 
 	 * Testiamo il costruttore del tavolo con tutti gli input validi
 	 */
-	
 	@Test
 	public void testCostruttoreTavolo() {
 		int numeroTavolo=1;
@@ -25,6 +24,21 @@ public class TestTavolo {
 		assertEquals(numeroTavolo, tavolo.getNumero());
 		assertEquals(maxCoperti, tavolo.getMaxCoperti());
 		assertEquals("Costo Errato",costoCoperto,tavolo.getCostoCoperto(),0);
+	}
+	
+	
+	
+	/* Test2
+	 * 
+	 * Testiamo il costruttore del tavolo numero tavolo non valido
+	 */
+	@Test(expected=InvalidInputException.class)
+	public void testCostruttoreNumeroTavoloZero() {
+		int numeroTavolo=0;
+		int maxCoperti=5;
+		double costoCoperto=2;
+		
+		tavolo = new Tavolo(numeroTavolo, maxCoperti, costoCoperto);
 	}
 
 }
