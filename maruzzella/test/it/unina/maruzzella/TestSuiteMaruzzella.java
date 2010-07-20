685d0ab661f002b622c077ffbeb4ba783fc46bfd
saverix <saverix@c82b02a6-e1a1-5eb2-583e-98feb170680a>
2010-07-20 13:46:13 +0000

    Inserita TestSuite

---
+++ maruzzella/test/it/unina/maruzzella/TestSuiteMaruzzella.java
@@ -0,0 +1,13 @@
+package it.unina.maruzzella;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { TestPiatto.class, TestTavolo.class })
+public class TestSuiteMaruzzella {
+	// the class remains completely empty,
+	// being used only as a holder for the above annotations
+}
