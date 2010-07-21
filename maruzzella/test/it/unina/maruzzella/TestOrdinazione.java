fd3456761f22ac08527f4b96fd3cad0e30d0762b
paco.dmp <paco.dmp@c82b02a6-e1a1-5eb2-583e-98feb170680a>
2010-07-21 14:48:12 +0000

    Aggiunta cglib

--- maruzzella/test/it/unina/maruzzella/TestOrdinazione.java
+++ maruzzella/test/it/unina/maruzzella/TestOrdinazione.java
@@ -3,21 +3,18 @@ package it.unina.maruzzella;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-import org.junit.runner.RunWith;
-import static org.junit.Assert.assertEquals;
-//import some necessary classes from the package of JMock2.2
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JMock;
-import org.jmock.integration.junit4.JUnit4Mockery;
-import org.jmock.Expectations;
+import static org.easymock.EasyMock.*;
 
-
-@RunWith(JMock.class)
 public class TestOrdinazione {
 
 	
 	@Test
 	public void testCostruttoreOrdinazione(){
+		ITavolo tavolo= createMock(Tavolo.class);
 		
+		
+		IOrdinazione ordinazione = new Ordinazione(tavolo);
+		
+		assertNotNull(ordinazione);
 	}
 }
