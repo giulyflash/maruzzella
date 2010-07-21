8bf949b246fb7b20211bc2311b02315bc0c6934b
paco.dmp <paco.dmp@c82b02a6-e1a1-5eb2-583e-98feb170680a>
2010-07-21 14:24:07 +0000

    TestOrdinazione in Produzione

---
+++ trunk/maruzzella/src/it/unina/maruzzella/Ordinazione.java
@@ -0,0 +1,26 @@
+package it.unina.maruzzella;
+
+import java.util.*;
+
+public class Ordinazione implements IOrdinazione {
+	private ITavolo tavolo;
+	private List<IPiatto> piatti;
+	
+	
+	public Ordinazione(ITavolo tavolo_associato){
+		
+	}
+	
+	@Override
+	public double calcolaConto() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void ordinaPiatto(IPiatto piatto_ordinato) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
