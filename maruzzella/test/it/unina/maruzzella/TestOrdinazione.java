ff7b304aa8bd468906332a885542d8a031915742
paco.dmp <paco.dmp@c82b02a6-e1a1-5eb2-583e-98feb170680a>
2010-07-21 14:58:26 +0000

    TestOrdinazione - Test2 => REDLINE

--- maruzzella/test/it/unina/maruzzella/TestOrdinazione.java
+++ maruzzella/test/it/unina/maruzzella/TestOrdinazione.java
@@ -7,6 +7,10 @@ import static org.easymock.EasyMock.*;
 
 public class TestOrdinazione {
 
+	/**Test1
+	 * 
+	 * Semplice test del construttore, ci aspettiamo un'istanza di Tavolo
+	 */
 	
 	@Test
 	public void testCostruttoreOrdinazione(){
@@ -17,4 +21,16 @@ public class TestOrdinazione {
 		
 		assertNotNull(ordinazione);
 	}
+	
+	/**Test2
+	 * 
+	 * test del construttore, passando parametro=null
+	 * Ci aspettiamo eccezione
+	 */
+	
+	@Test(expected=InvalidInputException.class)
+	public void testCostruttoreConNull() throws InvalidInputException{
+		IOrdinazione ordinazione = new Ordinazione(null);
+	}
+	
 }
