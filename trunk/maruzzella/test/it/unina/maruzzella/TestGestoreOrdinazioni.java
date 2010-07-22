3bafd6d59e8e209d0139144ffccc380d77616442
saverix <saverix@c82b02a6-e1a1-5eb2-583e-98feb170680a>
2010-07-22 13:11:23 +0000

    Classe TestGestoreOrdinazioni: INserito Test1 => GREENLINE

--- trunk/maruzzella/test/it/unina/maruzzella/TestGestoreOrdinazioni.java
+++ trunk/maruzzella/test/it/unina/maruzzella/TestGestoreOrdinazioni.java
@@ -1,9 +1,24 @@
 package it.unina.maruzzella;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import static org.easymock.EasyMock.*;
+
 
 public class TestGestoreOrdinazioni {
 	
-	
-	
+	/**Test1
+	 * 
+	 * Semplice test del construttore
+	 */
+	@Test
+	public void testCostruttoreOrdinazione()throws InvalidInputException{
+		
+		IGestoreOrdinazioni gestOrdinazioni = new GestoreOrdinazioni();
+		
+		assertNotNull(gestOrdinazioni);
+	}
 
 }
