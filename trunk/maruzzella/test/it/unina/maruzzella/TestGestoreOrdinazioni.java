dfa03a3e7325eca13c9790b45e7927dff32045b5
paco.dmp <paco.dmp@c82b02a6-e1a1-5eb2-583e-98feb170680a>
2010-07-24 08:42:55 +0000

    -Test6 - Errore Missing Behaviour

--- trunk/maruzzella/test/it/unina/maruzzella/TestGestoreOrdinazioni.java
+++ trunk/maruzzella/test/it/unina/maruzzella/TestGestoreOrdinazioni.java
@@ -113,19 +113,86 @@ public class TestGestoreOrdinazioni {
 		ITavoloCreator tavoloCreator = createMock(TavoloCreator.class);
 		IOrdinazioneCreator ordinazioneCreator = createMock(OrdinazioneCreator.class);
 		
+		//Record and playback
 		expect(tavoloCreator.creaTavolo(EasyMock.geq(1),EasyMock.eq(maxCoperti), EasyMock.eq(costoCoperto))).andReturn(tavolo);
 		expect(tavolo.isLibero()).andReturn(true);
 		expect(tavolo.getMaxCoperti()).andReturn(maxCoperti);
+		tavolo.setCoperti(maxCoperti);
+		
 		expect(ordinazioneCreator.creaOrdinazione(tavolo)).andReturn(ordinazione);
+		expect(tavolo.getNumero()).andReturn(numeroTavolo);
 		
 		gestOrdinazioni.setTavoloCreator(tavoloCreator);
 		gestOrdinazioni.setOrdinazioneCreator(ordinazioneCreator);
 		
 		replay(tavolo, tavoloCreator);
+		//end
+		
 		gestOrdinazioni.aggiungiTavolo(maxCoperti, costoCoperto);
 		int numeroTavoloRestituito = gestOrdinazioni.creaOrdinazione(maxCoperti);
 		verify(tavolo, tavoloCreator);
 		
 		assertEquals(numeroTavolo, numeroTavoloRestituito);
 	}
+	
+	
+	/**Test6
+	 * 
+	 */
+	@Test
+	public void testCreaOrdinazioneConPrimoTavoloOccupato()throws InvalidInputException{
+		
+		IGestoreOrdinazioni gestOrdinazioni = new GestoreOrdinazioni();
+		
+		int numeroTavolo1 = 1;
+		int numeroTavolo2 = 2;
+		
+		int maxCoperti1 = 7;
+		int maxCoperti2 = 2;
+		double costoCoperto1 = 2;
+		double costoCoperto2 = 2;
+		
+		ITavolo tavolo1 = createMock(Tavolo.class);
+		ITavolo tavolo2 = createMock(Tavolo.class);
+		IOrdinazione ordinazione1 = createMock(Ordinazione.class);
+		IOrdinazione ordinazione2 = createMock(Ordinazione.class);
+		
+		ITavoloCreator tavoloCreator = createMock(TavoloCreator.class);
+		IOrdinazioneCreator ordinazioneCreator = createMock(OrdinazioneCreator.class);
+		
+		//Record and playback
+		expect(tavoloCreator.creaTavolo(numeroTavolo1,maxCoperti1,costoCoperto1)).andReturn(tavolo1);
+		expect(tavoloCreator.creaTavolo(numeroTavolo2,maxCoperti2,costoCoperto2)).andReturn(tavolo2);
+		
+		//Ordinazione 1
+		expect(tavolo1.isLibero()).andReturn(true);
+		expect(tavolo1.getMaxCoperti()).andReturn(maxCoperti1);
+		tavolo1.setCoperti(maxCoperti1);
+		expect(ordinazioneCreator.creaOrdinazione(tavolo1)).andReturn(ordinazione1);
+		expect(tavolo1.getNumero()).andReturn(numeroTavolo1);
+		
+		//Ordinazione 2
+		expect(tavolo1.isLibero()).andReturn(false);
+		expect(tavolo2.isLibero()).andReturn(true);
+		expect(tavolo2.getMaxCoperti()).andReturn(maxCoperti2);
+		tavolo2.setCoperti(maxCoperti2);
+		expect(ordinazioneCreator.creaOrdinazione(tavolo2)).andReturn(ordinazione2);
+		expect(tavolo2.getNumero()).andReturn(numeroTavolo2);
+		
+		gestOrdinazioni.setTavoloCreator(tavoloCreator);
+		gestOrdinazioni.setOrdinazioneCreator(ordinazioneCreator);
+		
+		replay(tavolo1, tavolo2, tavoloCreator);
+		//end
+		
+		gestOrdinazioni.aggiungiTavolo(maxCoperti1, costoCoperto1);
+		gestOrdinazioni.aggiungiTavolo(maxCoperti2, costoCoperto2);
+		int numeroTavoloRestituito1 = gestOrdinazioni.creaOrdinazione(maxCoperti1);
+		int numeroTavoloRestituito2 = gestOrdinazioni.creaOrdinazione(maxCoperti2);
+		verify(tavolo1,tavolo2, tavoloCreator);
+		
+		assertEquals(numeroTavolo1, numeroTavoloRestituito1);
+		assertEquals(numeroTavolo2, numeroTavoloRestituito2);
+	}
 }
+
